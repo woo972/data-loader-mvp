703c05cfc44ee423cb876f591664eb44f3cc834d
woo972 <wzak9720>
2020-10-17 18:34:16 +0900

    20201017

--- src/main/java/com/wowls/dms/repository/WeatherRepository.java
+++ src/main/java/com/wowls/dms/repository/WeatherRepository.java
@@ -10,17 +10,32 @@ import java.util.Optional;
 
 public interface WeatherRepository extends JpaRepository<Weather, Long> {
     @Query(value = "SELECT weather_id, weather_data FROM weather" +
-            " WHERE weather_data->'$.date' >= :start_date" +
-            " AND weather_data->'$.date' <= :end_date" +
-            " AND weather_data->'$.location_code' = :location_code" +
-            " ORDER BY weather_data->'$.date'",
+            " WHERE json_value(weather_data,'$.date') >= :start_date" +
+            " AND json_value(weather_data,'$.date') <= :end_date" +
+            " AND json_value(weather_data, '$.location_code') = :location_code" +
+            " ORDER BY json_value(weather_data, '$.date')",
             countQuery = "SELECT count(*) FROM weather" +
-                    " WHERE weather_data->'$.date' >= :start_date" +
-                    " AND weather_data->'$.date' <= :end_date" +
-                    " AND weather_data->'$.location_code' = :location_code",
+                    " WHERE json_value(weather_data, '$.date') >= :start_date" +
+                    " AND json_value(weather_data, '$.date') <= :end_date" +
+                    " AND json_value(weather_data, '$.location_code') = :location_code",
             nativeQuery = true)
     Optional<Page<Weather>> findByDateAndLocationCode(@Param("start_date") String startDate,
                                                       @Param("end_date") String endDate,
                                                       @Param("location_code") String locationCode,
                                                       Pageable paging);
+//    MySQL
+//    @Query(value = "SELECT weather_id, weather_data FROM weather" +
+//            " WHERE weather_data->'$.date' >= :start_date" +
+//            " AND weather_data->'$.date' <= :end_date" +
+//            " AND weather_data->'$.location_code' = :location_code" +
+//            " ORDER BY weather_data->'$.date'",
+//            countQuery = "SELECT count(*) FROM weather" +
+//                    " WHERE weather_data->'$.date' >= :start_date" +
+//                    " AND weather_data->'$.date' <= :end_date" +
+//                    " AND weather_data->'$.location_code' = :location_code",
+//            nativeQuery = true)
+//    Optional<Page<Weather>> findByDateAndLocationCode(@Param("start_date") String startDate,
+//                                                      @Param("end_date") String endDate,
+//                                                      @Param("location_code") String locationCode,
+//                                                      Pageable paging);
 }
